c7392a71c318957e40729d84017447415c942dfc
tandcode <tandcode@gmail.com>
2023-12-22 10:20:49 +0200

    2023 day 09

---
+++ src/main/scala/com/tandcode/adventofcode/y2023/d09/MirageMaintenance.scala
@@ -0,0 +1,27 @@
+package com.tandcode.adventofcode.y2023.d09
+
+import com.tandcode.adventofcode.y2023.io.Util.strToLines
+
+import scala.annotation.tailrec
+
+object MirageMaintenance {
+
+  private def processValuesWithTransform(input: String, transform: List[Long] => List[Long]) = {
+    val lines = strToLines(input)
+      .map(line => transform(line.split("\\s+").map(_.toLong).toList))
+
+    @tailrec
+    def loop(difs: List[Seq[Long]]): Long =
+      if difs.head.forall(_ == 0)
+      then difs.tail.foldLeft(0L) { case (res, next) => next.head + res }
+      else loop(difs.head.sliding(2).map { case Seq(a, b) => a - b }.toSeq :: difs)
+
+    lines
+      .map(nums => loop(List(nums)))
+      .sum
+  }
+
+  def part1(input: String): Long = processValuesWithTransform(input, _.reverse)
+
+  def part2(input: String): Long = processValuesWithTransform(input, identity)
+}
