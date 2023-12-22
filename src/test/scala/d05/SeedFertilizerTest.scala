e9b14804f76278d5a32e7080f358f0c2a4ca80a4
tandcode <tandcode@gmail.com>
2023-12-22 09:37:22 +0200

    2023 day 05 finally done

--- src/test/scala/d05/SeedFertilizerTest.scala
+++ src/test/scala/d05/SeedFertilizerTest.scala
@@ -11,20 +11,26 @@ class SeedFertilizerTest extends AnyFlatSpec with Matchers {
 
   val test: String = strFromResource("y2023/test05.txt")
   val input: String = strFromResource("y2023/input05.txt")
+  val danyInput: String = strFromResource("y2023/test05v2.txt")
 
   "part1" should "pass with test data" in {
     part1(test) should be(35)
   }
 
   "part1" should "pass with input data" in {
-    part1(input) should be(318728750)
+    part1(input) should be(318728750L)
   }
 
   "part2" should "pass with test data" in {
-    part2(test) should be(2286)
+    part2(test) should be(46)
   }
 
   "part2" should "pass with input data" in {
-    part2(input) should be(79315)
+    part2(input) should be(37384986L)
   }
+
+  "part2" should "pass with dany data" in {
+    part2(danyInput) should be(2008785)
+  }
+
 }
