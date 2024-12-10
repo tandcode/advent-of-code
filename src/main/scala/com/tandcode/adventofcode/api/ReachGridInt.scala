477be30690b9a8c9d88301a831a65b9fe2216f57
tandcode <tandcode@gmail.com>
2024-12-10 21:21:09 +0200

    2024 day 10

--- src/main/scala/com/tandcode/adventofcode/api/ReachGridInt.scala
+++ src/main/scala/com/tandcode/adventofcode/api/ReachGridInt.scala
@@ -5,4 +5,8 @@ implicit class ReachGridInt(grid: Array[Array[Int]]) {
   def apply(pos: Pos): Int = grid(pos.y)(pos.x)
   def apply(pos: HeadingPos): Int = apply(pos.pos)
   
+  def findPos(v: Int): Seq[Pos] = {
+    grid.indices.flatMap(y => grid(y).indices.map(x => Pos(y, x)).filter(p => grid(p) == v))
+  }
+  
 }
---
+++ src/main/scala/com/tandcode/adventofcode/y2024/Day10.scala
@@ -0,0 +1,29 @@
+package com.tandcode.adventofcode.y2024
+import com.tandcode.adventofcode.io.ReachString
+import com.tandcode.adventofcode.io.ReachStrings
+import com.tandcode.adventofcode.api.ReachGridInt
+import com.tandcode.adventofcode.api.Pos
+
+object Day10 {
+
+  def part1(input: String): Long = {
+    val grid = input.slines.map(_.split("").numbers)
+    val startPositions = grid.findPos(0)
+    startPositions.map(p => countRoutes(grid, Seq(p)).toSet.size).sum
+  }
+
+  def part2(input: String): Long = {
+    val grid = input.slines.map(_.split("").numbers)
+    val startPositions = grid.findPos(0)
+    countRoutes(grid, startPositions).size
+  }
+
+  def countRoutes(grid: Array[Array[Int]], positions: Seq[Pos]): Seq[Pos] = {
+    val ends = positions.filter(p => grid(p) == 9)
+    val moreEnds = positions.flatMap(p => {
+      countRoutes(grid, p.steps.filter(next => next.validForGrid(grid) && grid(p) <= 9 && grid(next) == grid(p) + 1))
+    })
+    ends ++ moreEnds
+  }
+
+}
